0f3dc47e34e34553b5552cfaa1674cece726cb91
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-28 13:31:50 -0500

    pruebas del arbol avl

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
-
-
+import exercises.Exercises;
+import datastructures.AVLTree;
 public class Main {
+    public static void main(String[] args) {   
+        System.out.println(Exercises.isBalanced("{[()]}")); // YES,NO,YES
+        System.out.println(Exercises.isBalanced("[(])"));
+        System.out.println(Exercises.isBalanced("[[(())]]"));
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-
+        AVLTree<Integer> tree = new AVLTree<Integer>();
+        tree.insert(12);
+        tree.insert(34);
+        tree.insert(10);
+        tree.insert(14);
+        tree.insert(8);
+        tree.insert(11);
+        tree.insert(13);
+        System.out.println(tree.postOrden());
+        System.out.println(tree.get(14));
+        System.out.println(tree.size());
+        System.out.println(tree.height());
     }
 }
