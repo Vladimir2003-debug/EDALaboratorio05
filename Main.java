79abfa2c3469bdebf570408873dbfa5d41619080
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-29 11:52:59 -0500

    pruebas de los ejercicios 1 y 3

--- Main.java
+++ Main.java
@@ -13,9 +13,8 @@ public class Main {
         tree.insert(14);
         tree.insert(8);
         tree.insert(11);
-        tree.insert(13);
+
         System.out.println(tree.postOrden());
-        System.out.println(tree.get(14));
         System.out.println(tree.size());
         System.out.println(tree.height());
     }
